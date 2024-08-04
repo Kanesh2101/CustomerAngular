a9f2caea06cfab9b124d92e33d14d2dee64cdf1c
Kanesh2101 <Jkanesh2101@gmail.com>
2024-08-05 00:01:14 +0800

    added form and form validation

--- CustomerApp/src/app/modules/customer-management/customer-details/customer-details.component.ts
+++ CustomerApp/src/app/modules/customer-management/customer-details/customer-details.component.ts
@@ -1,10 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { Customer } from '../../types/customer.type';
+import { CommonModule } from '@angular/common';
+import { C } from '@angular/cdk/keycodes';
 
 @Component({
   selector: 'app-customer-details',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule, ReactiveFormsModule],
   templateUrl: './customer-details.component.html',
   styleUrl: './customer-details.component.scss',
 })
-export class CustomerDetailsComponent {}
+export class CustomerDetailsComponent implements OnInit {
+  customerForm!: FormGroup;
+  customer: Customer = {
+    email: '',
+    firstName: '',
+    lastName: '',
+    contactNo: 0,
+    address: '',
+  };
+
+  ngOnInit(): void {
+    this.customerForm = new FormGroup({
+      firstName: new FormControl('', [Validators.required]),
+      lastName: new FormControl('', [Validators.required]),
+      email: new FormControl('', [
+        Validators.required,
+        Validators.minLength(4),
+        Validators.pattern(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/),
+      ]),
+      contactNo: new FormControl('', [
+        Validators.required,
+        Validators.minLength(12),
+        Validators.pattern(/^(\+?6?01)[0-46-9]-*[0-9]{7,8}$/),
+      ]),
+      address: new FormControl('', [
+        Validators.required,
+        Validators.minLength(10),
+      ]),
+    });
+  }
+
+  onSubmit() {
+    this.customerForm.markAllAsTouched();
+
+    console.log(this.customerForm.value);
+  }
+}
---
+++ CustomerApp/src/app/modules/types/customer.type.ts
@@ -0,0 +1,7 @@
+export interface Customer {
+  email: string;
+  firstName: string;
+  lastName: string;
+  contactNo: number;
+  address: string;
+}
